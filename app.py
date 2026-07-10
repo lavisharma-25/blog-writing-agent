eb96e404a1191b250b95990c1253707932c4acff
Lavish Sharma <lavish.sh25@gmail.com>
2026-07-10 18:46:27 +0530

    add base project structure

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "backend.main:app",
+        host=os.getenv("HOST", "0.0.0.0"),
+        port=int(os.getenv("PORT", 5030)),
+        reload=True,
+    )
