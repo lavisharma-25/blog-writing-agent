9ff5bbe2b910f31977ef8d2dbf3d7c01e3d777bf
Lavish Sharma <lavish.sh25@gmail.com>
2026-08-10 11:54:15 +0530

    feat: Implement generate_blog function and workflow execution for blog generation

--- backend/src/api/endpoints/blogs.py
+++ backend/src/api/endpoints/blogs.py
@@ -5,12 +5,12 @@ from backend.src.api.functions.read_blog import read_blog
 from backend.src.api.functions.list_blogs import list_blogs
 from backend.src.api.functions.delete_blog import delete_blog
 from backend.src.api.functions.export_blog import export_blog
-# from backend.src.api.functions.generate_blog import generate_blog
+from backend.src.api.functions.generate_blog import generate_blog
 
 
 router = APIRouter()
 
-# router.post("/blogs/generate", response_model=schemas.WorkflowResponse, status_code=status.HTTP_200_OK)(generate_blog)
+router.post("/blogs/generate", response_model=schema.WorkflowResponse, status_code=status.HTTP_200_OK)(generate_blog)
 router.get("/blogs/list", response_model=schema.BlogListResponse, status_code=status.HTTP_200_OK)(list_blogs)
 router.post("/blogs/read", response_model=schema.ReadBlogResponse, status_code=status.HTTP_200_OK)(read_blog)
 router.delete("/blogs/delete", response_model=schema.DeleteBlogResponse, status_code=status.HTTP_200_OK)(delete_blog)
